abc7ed18c85951a9badc36c04abf649c57c802bd
Max Melching <m-melching@web.de>
2025-02-18 23:11:00 +0100

    Adding generation for data.

--- eccentric_template_generator.py
+++ eccentric_template_generator.py
@@ -46,3 +46,44 @@ plt.show()
 
 from gw_generator import signal_export
 signal_export(nr_series_tapered, 'eccentric_template.txt')
+
+
+noise_file = np.loadtxt('/home/user/Documents/GitHub/gwbar/noise.txt')
+noise_times = noise_file[:, 0]
+noise_data = noise_file[:, 1]
+
+noise_series = TimeSeries(noise_data, times=noise_times)
+
+# noise_series = noise_series.resample(nr_series_tapered.sample_rate)
+
+# # noisy_nr_series = nr_series_cropped.inject(noise_series)
+# # offset=len(noise_series)//8
+# noise_series = noise_series.resample(4096*u.Hz)
+# offset = 10
+# # reweight = noise_series.abs().max()
+# reweight = nr_series_tapered.abs().max()
+# # reweight = noise_series.abs().max() / nr_series_tapered.abs().max()
+# inject_noise = reweight*TimeSeries(noise_series[offset:offset+len(nr_series_cropped)], times=nr_series_cropped.times)
+# -- Maximum of eccentric is at 0.3, we have to reduce for nice looking output
+
+reweight = nr_series_tapered.abs().max()
+inject_noise = reweight * TimeSeries(noise_series.value[::-1], times=np.linspace(nr_series_cropped.times[0], nr_series_cropped.times[-1], num=len(noise_series)))
+inject_noise = inject_noise.resample(1./nr_series_cropped.dt)
+# -- For some reason, reverse makes things look much more realistic
+
+
+# inject_noise = inject_noise.resample(4096*u.Hz)
+noisy_nr_series = nr_series_cropped[1:] + inject_noise
+noisy_nr_series_tapered = noisy_nr_series * taper_window[1:]
+
+plt.plot(inject_noise)
+plt.plot(nr_series_tapered)
+plt.plot(noisy_nr_series_tapered)
+
+plt.show()
+
+
+normalize = noisy_nr_series_tapered.abs().max()
+
+signal_export(noisy_nr_series_tapered/normalize, '/home/user/Documents/GitHub/gwbar/eccentric_template_w_noise.txt', normalize_amplitude=False)
+signal_export(nr_series_tapered/normalize, '/home/user/Documents/GitHub/gwbar/eccentric_template_as_data.txt', normalize_amplitude=False)
--- gw_generator.py
+++ gw_generator.py
@@ -59,7 +59,7 @@ print(h.times)
 
 # -- Before exporting: rescale signal to have maximum amplitude of 1
 # -- and times between 0 and 1. This is assumed in plotting routine.
-def signal_export(signal: TimeSeries, name: str) -> None:
+def signal_export(signal: TimeSeries, name: str, normalize_amplitude: bool=True, normalize_times=True) -> None:
     """
     Export GW signal in time domain for format required by gwbar.
 
@@ -69,10 +69,75 @@ def signal_export(signal: TimeSeries, name: str) -> None:
         The signal to export.
     """
     signal_export = signal.copy()
-    signal_export /= signal_export.abs().max()
-    signal_export.times = np.linspace(0, 1, num=signal_export.size, endpoint=True)
+    if normalize_amplitude:
+        signal_export /= signal_export.abs().max()
+    if normalize_times:
+        signal_export.times = np.linspace(0, 1, num=signal_export.size, endpoint=True)
 
     np.savetxt(name, np.transpose([signal_export.times, signal_export.data]))
 
 # signal_export(h, 'generic_template.txt')
 signal_export(h_tapered, 'generic_template.txt')
+
+
+data = np.loadtxt('/home/user/Documents/GitHub/gwbar/fig1-observed-H.txt')
+times_data = data[:, 0]
+h_data = data[:, 1]
+
+
+h_series = TimeSeries(h_data, times=times_data)
+h_series_peak = np.argmax(h_series)
+
+
+
+data = np.loadtxt('/home/user/Documents/GitHub/gwbar/fig2-unfiltered-waveform-H.txt')
+times_template = data[:, 0]
+h_template = data[:, 1]
+
+h_template_series = TimeSeries(
+    h_template,
+    times=times_template
+    # t0=h_series.t0,
+    # dt=h_series.dt,
+)
+
+from scipy.signal import windows
+taper_window = windows.tukey(len(h_template_series), alpha=.25)
+h_template_series = h_template_series * taper_window
+
+
+taper_window_2 = windows.tukey(len(h_series), alpha=.25)
+h_series = h_series * taper_window_2
+
+
+
+# -- Store noise realization (with high sampling rate)
+signal_export(h_series[1:] - h_template_series, '/home/user/Documents/GitHub/gwbar/noise.txt', normalize_amplitude=False, normalize_times=False)
+
+
+# print(h_series.dt, h_template_series.dt)
+# print(h_series.sample_rate, h_template_series.sample_rate)
+
+# h_series = h_series.resample(4096*u.Hz)
+# h_template_series = h_template_series.resample(4096*u.Hz)
+
+# print(h_series)
+# # TODO: resample
+
+# plt.figure(figsize=(12, 6))
+# plt.plot(h_series)
+# # plt.vlines(h_series.times.value[h_series_peak], -1, 1, color='red')
+# plt.plot(h_template_series)
+# plt.show()
+
+
+
+
+normalize = max(h_series.abs().max(), h_template_series.abs().max())
+
+h_series /= normalize
+h_template_series /= normalize
+
+
+signal_export(h_series, '/home/user/Documents/GitHub/gwbar/h_data.txt', normalize_amplitude=False)
+signal_export(h_template_series, '/home/user/Documents/GitHub/gwbar/h_template.txt', normalize_amplitude=False)
